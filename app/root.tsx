ea5fc0cea32a7574ce0cc25ed1650fe3402a9527
bodiddlie <bodiddlie@gmail.com>
2021-12-21 21:25:01 -0700

    get app working properly locally, add error handlers to root

--- app/root.tsx
+++ app/root.tsx
@@ -4,6 +4,7 @@ import {
   LoaderFunction,
   Outlet,
   Scripts,
+  useCatch,
   useLoaderData,
 } from 'remix';
 
@@ -11,6 +12,7 @@ import styles from './tailwind.css';
 import { GiGamepadCross } from 'react-icons/gi';
 import { getUserEmail } from './util/session.server';
 import { Form } from '@remix-run/react';
+import * as React from 'react';
 
 export function links() {
   return [{ rel: 'stylesheet', href: styles }];
@@ -49,6 +51,62 @@ export default function App() {
         </div>
         <Scripts />
         {process.env.NODE_ENV === 'development' ? <LiveReload /> : null}
+      </body>
+    </html>
+  );
+}
+
+export function ErrorBoundary({ error }: { error: Error }) {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Pile of Shame</title>
+      </head>
+      <body>
+        <div className="flex flex-col min-h-screen">
+          <div className="p-2 flex w-full justify-between bg-pale-cream align-middle">
+            <div className="text-2xl text-red-700 flex items-center">
+              <GiGamepadCross />
+              <span>Pile of Shame</span>
+            </div>
+          </div>
+          <div className="flex-grow flex flex-col justify-center items-center">
+            <h3 className="text-gray-800 border border-gray-800 bg-yellow-200 p-2 rounded-2xl m-2">
+              There's been an error.
+            </h3>
+            <p>{error.message}</p>
+          </div>
+        </div>
+      </body>
+    </html>
+  );
+}
+
+export function CatchBoundary() {
+  const caught = useCatch();
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Pile of Shame</title>
+      </head>
+      <body>
+        <div className="flex flex-col min-h-screen">
+          <div className="p-2 flex w-full justify-between bg-pale-cream align-middle">
+            <div className="text-2xl text-red-700 flex items-center">
+              <GiGamepadCross />
+              <span>Pile of Shame</span>
+            </div>
+          </div>
+          <div className="flex-grow flex flex-col justify-center items-center">
+            <h3 className="text-gray-800 border border-gray-800 bg-yellow-200 p-2 rounded-2xl m-2">
+              {caught.status} {caught.statusText}
+            </h3>
+          </div>
+        </div>
       </body>
     </html>
   );
